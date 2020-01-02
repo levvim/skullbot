83adb2f4b2b7027b674dc923fa1eca3a42701b48
Mangarin <marksemailaddressishere@gmail.com>
2020-01-02 16:41:07 -0500

    json parser to clean data

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # skullbot
 ai for skullnrose
+
+
+1. export db to json
+2. [,,]
+3. clean_data.py
---
+++ clean_data.py
@@ -0,0 +1,54 @@
+# levvim 2019
+# python clean_data.py -i data/SNR_train_3p.json -o data/SNR_train_3p_clean.csv
+################################################################################
+import argparse
+import pandas as pd
+import json
+from pprint import pprint
+from pandas.io.json import json_normalize 
+
+# parser
+parser = argparse.ArgumentParser()
+parser.add_argument("--input", "-i", help="input file")
+parser.add_argument("--output", "-o", help="output file")
+args = parser.parse_args()
+
+# load in file and clean up
+with open(args.input, encoding='utf-8-sig') as f_input:
+        d = pd.read_json(f_input, orient='values')
+
+## split columns that are list of dictionaries
+d_temp=pd.DataFrame(d.userOthers.values.tolist(), index=d.index)
+d_temp.columns=['userOthers' + str(col) for col in d_temp.columns]
+d=pd.concat([d,d_temp], axis=1).reset_index(drop=True)
+
+d=d.filter(items=['userCurrent', 'userOthers0','userOthers1','userState']) # filter to col
+d=d[~d['userOthers1'].isnull()] # remove null values for dropout (need to fix later
+
+# split dictionary columns
+
+## normalize 1 column
+#d2=d
+#d_temp = json_normalize(d['userCurrent'])
+#d_temp.columns = ['userCurrent_' + str(col) for col in d_temp.columns]
+#d2=pd.concat([d2,d_temp])
+#print(d_temp)
+
+## normalize all columns
+print(d)
+d2=pd.DataFrame()
+for col in d:
+    print(col)
+    d_col=d[col]
+    d_temp=json_normalize(d[col])
+    d_temp.columns = [col + '_' + str(i) for i in d_temp.columns]
+    d2=pd.concat([d2,d_temp], axis=1).reset_index(drop=True)
+
+print(d2.head(5))
+
+# write to file
+d2.to_csv(args.output, encoding='utf-8', index=False)
+
+#with open("toptree.txt", "a") as myfile:
+#    myfile.write("\n")
+#    myfile.write(json.dumps(d, indent=4))
